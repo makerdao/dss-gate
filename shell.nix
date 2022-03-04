991f0781b3d606805e39d005b6d8f3da079df032
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-03-04 10:16:25 -0700

    fix solc version

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
 }: with dappPkgs;
 
 mkShell {
-  DAPP_SOLC = solc-static-versions.solc_0_6_12 + "/bin/solc-0.8.0";
+  DAPP_SOLC = solc-static-versions.solc_0_8_0 + "/bin/solc-0.8.0";
   # No optimizations
   SOLC_FLAGS = "";
   buildInputs = [
